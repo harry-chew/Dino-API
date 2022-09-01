00af0eda6b643210507e6db3675d66d551936013
Harry Chew <harry@tech3.co.uk>
2022-09-01 21:37:00 +0100

    added logger, basic JSON database and added status codes to routes

--- routes/dinos.js
+++ routes/dinos.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../database');
+const dinodb = require('../dinodb');
+
 router.get('/', (req, res) => {
-    res.send('Get All Dinos');
+    res.status(200).send(dinodb);
 });
 
 router.post('/', (req, res) => {
-    const { name } = req.body;
-    if(name) {
-        console.log(name);
+    const { authorization } = req.headers;
+    if(authorization && authorization === '123') {
+        const dino = req.body;
+        dinodb.push(dino);
+        res.status(201).send(dinodb);
     }
+    else {
+        res.status(403).send('Forbidden');
+    }
+
 });
 
 router.get('/:id', (req, res) => {
--- server.js
+++ server.js
@@ -1,16 +1,45 @@
 const express = require('express');
 const app = express();
+const fs = require('fs/promises');
 
 app.set('view engine', 'ejs');
+
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+
+app.use(async (req, res, next) => {
+    const content = `${dateToISOLikeButLocal(new Date())} - ${req.method} - ${req.url} - AuthToken:${req.headers.authorization} \n`;
+    
+    await fs.appendFile('log.txt', content, { flag: 'a+' }, err => {
+        if (err) {
+          console.error(err);
+        }
+        console.log(`${Date()} - ${req.method} - ${req.url}`);
+      });
+    next();
+});
+
+const dateToISOLikeButLocal = (date) => {
+    const offsetMs = date.getTimezoneOffset() * 60 * 1000;
+    const msLocal =  date.getTime() - offsetMs;
+    const dateLocal = new Date(msLocal);
+    const iso = dateLocal.toISOString();
+    const isoLocal = iso.slice(0, 19);
+    return isoLocal;
+};
 
 app.get('/', (req, res) => {
     console.log("Hello World");
     res.send("Hello World");
 });
 
+
+
 const dinosRouter = require('./routes/dinos.js');
 app.use('/dino', dinosRouter);
 
-app.listen(8080, () => "App listening on port 8080");
+
+
+app.listen(8080, () => {
+    console.log("Server started on port 8080");
+});
