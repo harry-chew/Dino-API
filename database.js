61f624e7946198c49f425f5d0ef5fea2c7086e69
Harry Chew <harry@tech3.co.uk>
2022-09-02 08:08:15 +0100

    seperated db config variables

--- database.js
+++ database.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql2');
+const conf = require('./conf');
 
 
 module.exports = mysql.createConnection({
-    host: 'localhost',
-    user: 'dbuser',
-    password: 'dbpass',
-    database: 'dino'
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    database: conf.database
 });
