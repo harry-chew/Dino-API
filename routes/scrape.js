7bf486d88be3ad37fae995a644ed1d6e818fdcbd
Harry Chew <harry@tech3.co.uk>
2022-09-06 19:19:00 +0100

    working on api front end

--- routes/scrape.js
+++ routes/scrape.js
@@ -30,10 +30,31 @@ async function scrapePage(url) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
-    const element = await page.$x('/html/body/div[3]/h1/i');
-    const title = await(await element[0].getProperty('textContent')).jsonValue();
+    const title = await page.$x('/html/body/div[3]/h1/i');
+    const titleText = await(await title[0].getProperty('textContent')).jsonValue();
+
+    const image = await page.$x('//*[@id="mw-content-text"]/div[1]/table[1]/tbody/tr[2]/td/a/img');
+    const imageSrc = await(await image[0].getProperty('src')).jsonValue();
+
+    const info = await page.$x('//*[@id="mw-content-text"]/div[1]/p[2]');
+    const infoText = await(await info[0].getProperty('textContent')).jsonValue();
+
+    const desc = await page.$x('/html/body/div[3]/div[3]/div[5]/div[1]/p[5]');
+    const descText = await(await desc[0].getProperty('textContent')).jsonValue();
+
+    // for(let i = 0; i < info.length; i++) {
+    //     infoText.push(await(await info[i].getProperty('textContent')).jsonValue());
+    // }
+    
     await browser.close();
-    return title;
+
+    const returnObj = {
+        title : titleText,
+        img : imageSrc,
+        info : infoText,
+        desription : descText
+    };
+    return returnObj;
 };
 
 
